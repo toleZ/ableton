4ce492ad222f55a89b1e5b59a1d52c580f12d45e
Juan C. Toloy <toloyjc@gmail.com>
2023-07-09 19:02:13 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ableton: {
+          fbffa7: "#fbffa7",
+          ff764d: "#ff764d",
+          b1c5ff: "#b1c5ff",
+          d5b3ff: "#d5b3ff",
+          b6ffc0: "#b6ffc0",
+          "0000ff": "#0000ff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
