bd375aee1bd23ecbb5a687e0ee378690182cdf57
Juan C. Toloy <toloyjc@gmail.com>
2023-07-10 09:17:50 -0300

    fix & improvements: solving bugs with positions, adding bg-f3f3f3 & adding images

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="max-w-[1600px] mx-auto">
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<h1>Not found</h1>} />
-        </Routes>
-      </BrowserRouter>
+    <div className="bg-[#f3f3f3]">
+      <div className="max-w-[1600px] mx-auto bg-white">
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<h1>Not found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import { IoIosArrowForward } from "react-icons/io";
 const Home = () => {
   return (
     <main>
-      <section className="relative font-semibold">
-        <article className="absolute top-0 left-0 w-[60vw] max-w-[900px] h-[40vw] max-h-[600px] p-12 bg-[url('/img/403_Product_Takeover2_2BftvZt.avif')] bg-cover bg-no-repeat bg-center text-ableton-fdf900">
+      <section className="font-semibold">
+        <article className="w-[60vw] max-w-[900px] h-[40vw] max-h-[600px] p-12 bg-[url('/img/403_Product_Takeover2_2BftvZt.avif')] bg-cover bg-no-repeat bg-center text-ableton-fdf900">
           <a href="#">
             <h2 className="text-4xl lg:text-5xl lg:mb-4">
               Your computer is your instrument
@@ -14,7 +14,7 @@ const Home = () => {
             </p>
           </a>
         </article>
-        <article className="absolute top-[10rem] lg:top-[20rem] right-0 w-[33vw] lg:w-[27vw] lg:h-[32vh] h-[20vh] p-6 bg-[url('/img/Note.avif')] bg-cover bg-no-repeat bg-center text-ableton-00ffff">
+        <article className="translate-x-[215%] -translate-y-full w-[33vw] lg:w-[27vw] h-[20vh] lg:h-[32vh] p-6 bg-[url('/img/Note.avif')] bg-cover bg-no-repeat bg-center text-ableton-00ffff">
           <a href="#">
             <h2 className="text-base lg:text-3xl">Make new ideas a habit</h2>
             <p className="flex items-center gap-1 text-base lg:text-xl underline">
@@ -22,7 +22,7 @@ const Home = () => {
             </p>
           </a>
         </article>
-        <article className="absolute top-[20rem] lg:top-[34rem] left-[60%] -translate-x-1/2 w-[33vw] lg:w-[27vw] lg:h-[32vh] h-[20vh] p-6 bg-[url('/img/Push-opt2.avif')] bg-cover bg-no-repeat bg-center text-ableton-00ffff">
+        <article className="translate-x-[125%] -translate-y-[125%] w-[33vw] lg:w-[27vw] h-[20vh] lg:h-[32vh] p-6 bg-[url('/img/Push-opt2.avif')] bg-cover bg-no-repeat bg-center text-ableton-00ffff">
           <a href="#">
             <h2 className="text-base lg:text-3xl">Focus and feel</h2>
             <p className="flex items-center gap-1 text-base lg:text-xl underline">
